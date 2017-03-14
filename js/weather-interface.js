b15f9623692f31a6edee55ebf174a32209905e8b
Ash Laidlaw and Felix Oporto Lopez <ash@>
2017-03-14 15:16:47 -0700

    Add comparisons

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,17 +1,25 @@
 var Weather = require("./../js/weather.js").weatherModule;
 
-
 $(function() {
   var currentWeatherObject = new Weather();
+
+  $("#temp").click(function() {
+    var city = $("#city").val();
+    $("#city").val("");
+    currentWeatherObject.getTemp(city);
+  });
+
   $("#humidity").click(function() {
     var city = $("#city").val();
     $("#city").val("");
     currentWeatherObject.getHumidity(city);
   });
 
-  $("#temp").click(function() {
-    var city = $("#city").val();
-    $("#city").val("");
-    currentWeatherObject.getTemp(city);
+  $("#compare").click(function() {
+    var city1 = $("#city1").val();
+    var city2 = $("#city2").val();
+    $("#city1").val("");
+    $("#city2").val("");
+    currentWeatherObject.compareTemps(city1, city2);
   });
 });
--- js/weather.js
+++ js/weather.js
@@ -4,9 +4,8 @@ function Weather() {
 }
 
 Weather.prototype.getTemp = function(city) {
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
-  .then(function(response) {
-    $('#output').text("The temperature in " + city + " is " + (response.main.temp - 273.15).toFixed(0) + "°C and " + ((response.main.temp * 9/5 - 459.67).toFixed(0)) + "°F.");
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+    $("#output").text("The temperature in Fahrenheit in " + city + " is " + (response.main.temp * 9/5 - 459.67).toFixed(0) + "°F. The temperature in Celcius is " + (response.main.temp - 273.15).toFixed(0) + "°C.");
   }).fail(function(error) {
     $('#output').text(error.responseJSON.message);
   });
@@ -15,9 +14,26 @@ Weather.prototype.getTemp = function(city) {
 Weather.prototype.getHumidity = function(city) {
   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
   .then(function(response) {
-    $('#output').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    $("#output").text("The humidity in " + city + " is " + response.main.humidity + "%.");
   }).fail(function(error) {
     $('#output').text(error.responseJSON.message);
+  });
+};
+
+Weather.prototype.compareTemps = function(city1, city2) {
+
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city1 + '&appid=' + apiKey).then(function(response) {
+    var tempCity1 = response.main.temp;
+
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city2 + '&appid=' + apiKey).then(function(response2) {
+      var tempCity2 = response2.main.temp;
+
+      if(tempCity1 > tempCity2) {
+        $("#output").text(city1 + " is warmer than " + city2);
+      } else {
+        $("#output").text(city1 + " is cooler than " + city2);
+      }
+    });
   });
 };
 
